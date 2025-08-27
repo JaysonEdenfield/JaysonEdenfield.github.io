4101b356d62f9d9b7af561ca58110b0a7d8f835c
JaysonEdenfield <137231019+JaysonEdenfield@users.noreply.github.com>
2025-08-27 18:18:06 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const socket = io(); // todo setup serverside
+const message = getElementById("MsgIn");
+
+class user {
+    Name;
+    SendMessage() {
+        if (this.Message)
+            
+    }
+}
+
